ab8a15485b41049c296451df7f0ffb6d3155a4e2
Simon <simonfong6@gmail.com>
2019-05-12 14:55:48 -0700

    Prod files

---
+++ build/precache-manifest.15b1bef1b92a0a4b7061748eee29a701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5c9e3bd980aaa3c508827a764cbec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b96b759505f589caca00",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b595394dd6b301751926",
+    "url": "/static/js/2.232006d0.chunk.js"
+  },
+  {
+    "revision": "b96b759505f589caca00",
+    "url": "/static/js/main.3f58a91a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
