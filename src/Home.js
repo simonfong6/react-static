e5b2e79cac7f748a817d7c76bc4bef30089e16a8
Simon <simonfong6@gmail.com>
2019-05-12 18:05:28 -0700

    Removing submenue

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 // Home.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './images/ESW.png';
 import './assets/css/main.css';
 
@@ -10,26 +11,11 @@ class Home extends React.Component {
         <h1><img id="logo" src={logo} alt="ESW Logo"/></h1>
         <nav id="nav">
           <ul>
-            <li class="current"><a href="index.html">About Us</a></li>
-            <li class="submenu">
-              <a href="#">Menu</a>
-              <ul>
-                <li><a href="left-sidebar.html">Team</a></li>
-                <li><a href="right-sidebar.html">Updates</a></li>
-                <li><a href="no-sidebar.html">Media</a></li>
-                <li><a href="contact.html">Donate</a></li>
-                <li class="submenu">
-                  <a href="#">Submenu</a>
-                  <ul>
-                    <li><a href="#">Dolore Sed</a></li>
-                    <li><a href="#">Consequat</a></li>
-                    <li><a href="#">Lorem Magna</a></li>
-                    <li><a href="#">Sed Magna</a></li>
-                    <li><a href="#">Ipsum Nisl</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
+            <li class="current"><Link to="/">About Us</Link></li>
+            <li><a href="left-sidebar.html">Team</a></li>
+            <li><a href="right-sidebar.html">Updates</a></li>
+            <li><Link to="/post/">Media</Link></li>
+            <li><a href="contact.html">Donate</a></li>
             <li><a href="#" class="button primary">Contact Us</a></li>
           </ul>
         </nav>
