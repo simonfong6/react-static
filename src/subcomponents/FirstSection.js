55db6237d55fcfac9165abf6a50c2b7df23337a3
Simon <simonfong6@gmail.com>
2019-05-13 16:54:50 -0700

    Major sections in components

---
+++ src/subcomponents/FirstSection.js
@@ -0,0 +1,23 @@
+// FirstSection.js
+import React from 'react';
+
+class FirstSection extends React.Component {
+  render() {
+    return (
+      <header class="special container">
+        <span class="icon fa-bar-chart-o"></span>
+        <h2>As this is my <strong>twentieth</strong> freebie for HTML5 UP
+        <br />
+        I decided to give it a really creative name.</h2>
+        <p>Turns out <strong>Twenty</strong> was the best I could come up with. Anyway, lame name aside,
+        <br />
+        it's minimally designed, fully responsive, built on HTML5/CSS3,
+        and, like all my stuff,
+        <br />
+        released for free under the <a href="http://html5up.net/license">Creative Commons Attribution 3.0</a> license. Have fun!</p>
+      </header>
+    );
+  }
+}
+
+export default FirstSection;
---
+++ src/subcomponents/Ready.js
@@ -0,0 +1,25 @@
+// Ready.js
+import React from 'react';
+
+class Ready extends React.Component {
+  render() {
+    return (
+      <section id="cta">
+
+        <header>
+          <h2>Ready to do <strong>something</strong>?</h2>
+          <p>Proin a ullamcorper elit, et sagittis turpis integer ut fermentum.</p>
+        </header>
+        <footer>
+          <ul class="buttons">
+            <li><a href="#" class="button primary">Take My Money</a></li>
+            <li><a href="#" class="button">LOL Wut</a></li>
+          </ul>
+        </footer>
+
+      </section>
+    );
+  }
+}
+
+export default Ready;
