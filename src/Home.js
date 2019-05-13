397e24185a2f810442cda6882ecd1cf5c58357c7
Simon <simonfong6@gmail.com>
2019-05-13 16:12:03 -0700

    Moved Navbar into its own component

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,15 @@
 // Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from './subcomponents/Navbar.js';
 import logo from './images/ESW.png';
 import './assets/css/main.css';
 
 class Home extends React.Component {
   render() {
-    return <div id="page-wrapper">
-      <header id="header" class="alt">
-        <h1><img id="logo" src={logo} alt="ESW Logo"/></h1>
-        <nav id="nav">
-          <ul>
-            <li class="current"><Link to="/">About Us</Link></li>
-            <li><a href="left-sidebar.html">Team</a></li>
-            <li><a href="right-sidebar.html">Updates</a></li>
-            <li><Link to="/post/">Media</Link></li>
-            <li><a href="contact.html">Donate</a></li>
-            <li><a href="#" class="button primary">Contact Us</a></li>
-          </ul>
-        </nav>
-      </header>
+    return (
+    <div id="page-wrapper">
+      <Navbar />
 
       <section id="banner">
 
@@ -222,7 +212,7 @@ class Home extends React.Component {
 
       </footer>
 
-  </div>;
+    </div>);
   }
 }
 
---
+++ src/subcomponents/Navbar.js
@@ -0,0 +1,28 @@
+// Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './../images/ESW.png';
+import './../assets/css/main.css';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <header id="header" class="alt">
+        <h1><img id="logo" src={logo} alt="ESW Logo"/></h1>
+        <nav id="nav">
+          <ul>
+            <li class="current"><Link to="/">About Us</Link></li>
+            <li><a href="left-sidebar.html">Team</a></li>
+            <li><a href="right-sidebar.html">Updates</a></li>
+            <li><Link to="/post/">Media</Link></li>
+            <li><a href="contact.html">Donate</a></li>
+            <li><a href="#" class="button primary">Contact Us</a></li>
+          </ul>
+        </nav>
+      </header>
+
+    );
+  }
+}
+
+export default Navbar;
