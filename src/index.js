c6d397cc1a09b672b57110c648cc2926f0522305
Simon <simonfong6@gmail.com>
2019-05-13 21:44:25 -0700

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import { hydrate, render } from 'react-dom';
 import App from './App';
 const rootElement = document.getElementById('root');
