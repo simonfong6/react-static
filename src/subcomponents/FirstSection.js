37e3487fe1e647bee7d5f2a6e099eb6eed3c82be
Simon <simonfong6@gmail.com>
2019-05-13 21:53:15 -0700

    Some about

--- src/subcomponents/FirstSection.js
+++ src/subcomponents/FirstSection.js
@@ -6,15 +6,28 @@ class FirstSection extends React.Component {
     return (
       <header class="special container">
         <span class="icon fa-bar-chart-o"></span>
-        <h2>As this is my <strong>twentieth</strong> freebie for HTML5 UP
-        <br />
-        I decided to give it a really creative name.</h2>
-        <p>Turns out <strong>Twenty</strong> was the best I could come up with. Anyway, lame name aside,
-        <br />
-        it's minimally designed, fully responsive, built on HTML5/CSS3,
-        and, like all my stuff,
-        <br />
-        released for free under the <a href="http://html5up.net/license">Creative Commons Attribution 3.0</a> license. Have fun!</p>
+        <h2>A Student Organization Dedicated to Excellence in Sustainable Engineering</h2>
+        <p class="text-left my-3" >
+          Established in the fall of 2015, UCSD's first Solar Car Team is an
+          entirely student-run project that aims to design, build, test, and
+          race solar powered vehicles. Comprised of over 20 students across a
+          variety of disciplines and areas of study, we have come together to
+          start a Solar Car program at UCSD, located in one of the sunniest
+          cities in the world.
+        </p>
+        <p class="text-left my-3" >
+          The Solar Car Project is the first of its kind in that it attracts a
+          large variety of students from various engineering as well as
+          non-engineering backgrounds. Participating in this project gives
+          students the opportunity to gain hands-on experience with different
+          fields of engineering (mechanical, engineering, aerospace, design) as
+          well as experience in business, operations, and project management.
+          Students in Triton Solar Car can stand out from the crowd by gaining
+          important real-world experience and operating in an environment that
+          prepares them for their careers by getting involved in setting
+          deadlines, budgets, designs, reviews while doing their best to control
+          all the unforeseen variables that may arise. 
+        </p>
       </header>
     );
   }
