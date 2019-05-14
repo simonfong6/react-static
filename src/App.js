fb0475771fcb195125edca1174438888edbc733b
Simon <simonfong6@gmail.com>
2019-05-13 22:20:04 -0700

    Moved navbar and footer to general App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './subcomponents/Navbar.js';
+import Footer from './subcomponents/Footer.js';
 import Home from './Home';
 import Post from './Post';
 import './assets/css/font-awesome.min.css';
+import './assets/css/main.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/post/" component={Post} />
         </Switch>
+        <Footer />
       </div>
     </BrowserRouter>
     
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,14 @@
 // Home.js
 import React from 'react';
-import Navbar from './subcomponents/Navbar.js';
 import Banner from './subcomponents/Banner.js';
-import Footer from './subcomponents/Footer.js';
 import FirstSection from './subcomponents/FirstSection.js';
 import ThreeColumns from './subcomponents/ThreeColumns.js';
-import './assets/css/main.css';
 
 class Home extends React.Component {
   render() {
     return (
     <div id="page-wrapper">
-      <Navbar />
+
       <Banner />
 
       <article id="main">
@@ -19,7 +16,7 @@ class Home extends React.Component {
         <ThreeColumns />
       </article>
 
-      <Footer />
+
     </div>);
   }
 }
